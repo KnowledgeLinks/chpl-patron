70084137c40d4c2983cd4b3c71c88cde013449d7
Jeremy Nelson <jermnelson@gmail.com>
2016-11-07 20:02:40 -0700

    Added Unique constraint to email

--- db-schema.sql
+++ db-schema.sql
@@ -1,37 +1,8 @@
--- CREATE TABLE Patron (
---     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
---     first_name VARCHAR NOT NULL,
---     last_name VARCHAR NOT NULL,
---     birth_day VARCHAR NOT NULL
--- );
-
--- CREATE TABLE Location (
---     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
---     address VARCHAR NOT NULL,
---     city VARCHAR NOT NULL,
---     state VARCHAR NOT NULL,
---     zip_code VARCHAR NOT NULL
--- );
-
--- CREATE TABLE Email (
---     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
---     address VARCHAR NOT NULL
---     -- patron INTEGER,
---     -- FOREIGN KEY (patron) REFERENCES Patron (id)
--- );
-
--- CREATE TABLE Telephone (
---     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
---     number VARCHAR NOT NULL,
---     patron INTEGER,
---     FOREIGN KEY (patron) REFERENCES Patron (id)
--- );
-
 CREATE TABLE LibraryCardRequest (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     date DATETIME DEFAULT CURRENT_TIMESTAMP, 
     date_retrieved DATETIME,
     temp_number INTEGER,
-    email VARCHAR NOT NULL
+    email VARCHAR NOT NULL UNIQUE
 );
 
