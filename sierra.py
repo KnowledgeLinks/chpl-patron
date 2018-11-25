fd419daf3a2afa269f9d9d6998cbb4d79af7c470
Jeremy Nelson <jermnelson@gmail.com>
2018-11-24 20:53:27 -0700

    Added test for login to sierra

--- sierra.py
+++ sierra.py
@@ -2,13 +2,24 @@ import json
 import requests
 import instance
 
+SIERRA_API = "https://catalog.chapelhillpubliclibrary.org/iii/sierra-api/v5/"
+# SIERRA_API = "https://sandbox.iii.com:443/iii/sierra-api/v5/patrons/"
+
+PATRON_URL = "{0}patrons/".format(SIERRA_API)
+TOKEN_URL = "{0}token".format(SIERRA_API)
+
+
 def get_token():
-    header = {
-        "Authorization": "Basic {}".format(instance.API_KEY), 
-        "Content-Type": "application/x-www-form-urlencoded"
+    headers = {
+       "Authorization": "Basic {}".format(instance.API_KEY), 
+       "Content-Type": "application/x-www-form-urlencoded"
     }
-    response = requests.post(instance.TOKEN_URL, headers=header)
-    return response.json().get("access_token")
+    response = requests.post(instance.TOKEN_URL, headers=headers)
+    if response.status_code < 399:
+        return response.json().get("access_token")
+    raise ValueError("get_token request status code: {}".format(
+        response.status_code),
+        response.text)
 
 
 def register(payload):
@@ -16,4 +27,4 @@ def register(payload):
         "Authorization": "Bearer {}".format(get_token()),
         "Content-Type": "application/json"
     } 
-    result = requests.post(instance.PATRON_URL)
+    result = requests.posti(PATRON_URL)
