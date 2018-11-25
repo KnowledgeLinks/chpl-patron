cf47a2a8ecd68a9edbeba0b8d9bfa29244ba45a0
Jeremy Nelson <jermnelson@gmail.com>
2018-11-24 23:29:33 -0700

    Added b64 key

--- sierra.py
+++ sierra.py
@@ -1,20 +1,24 @@
+import base64
 import json
 import requests
 import instance
 
-# SIERRA_API = "https://catalog.chapelhillpubliclibrary.org/iii/sierra-api/v5/"
-SIERRA_API = "https://sandbox.iii.com:443/iii/sierra-api/v5/patrons/"
+SIERRA_API = "https://catalog.chapelhillpubliclibrary.org/iii/sierra-api/v5/"
+# SIERRA_API = "https://sandbox.iii.com:443/iii/sierra-api/v5/patrons/"
 
 PATRON_URL = "{0}patrons/".format(SIERRA_API)
 TOKEN_URL = "{0}token".format(SIERRA_API)
 
 
 def get_token():
+    encoded_key = base64.b64encode("{}:{}".format(instance.API_KEY, 
+        instance.CLIENT_SECRET).encode())
     headers = {
-       "Authorization": "Basic {}".format(instance.CLIENT_SECRET), 
+       "Authorization": "Basic {}".format(encoded_key), 
        "Content-Type": "application/x-www-form-urlencoded"
     }
     response = requests.post(TOKEN_URL, headers=headers)
+    import pdb; pdb.set_trace()
     if response.status_code < 399:
         return response.json().get("access_token")
     raise ValueError("{} get_token request status code: {}".format(
