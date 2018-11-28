02ef2b883494a992523dcbf9fe43c3aabf2c434e
Jeremy Nelson <jermnelson@gmail.com>
2018-11-27 20:28:33 -0700

    Now testing empty payload to register function

--- test_sierra.py
+++ test_sierra.py
@@ -13,5 +13,22 @@ class Test_get_token(unittest.TestCase):
     def tearDown(self):
         pass
 
+class Test_register(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_register_empty_payload(self):
+        result = sierra.register({})
+        self.assertEqual(result.status_code,
+                         400)
+        self.assertEqual(result.json().get('code'),
+                         130)
+        self.assertEqual(result.json().get('name'),
+            'Bad JSON/XML Syntax')
+
+    def tearDown(self):
+        pass
+
 if __name__ == '__main__':
     unittest.main()
